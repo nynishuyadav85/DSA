bd368f2e3a928a8459dc39aaefba04bfcb530c6d
Nishant Yadav <nynishuyadav85@gmail.com>
2025-06-14 23:55:49 +0530

    diffrebt case

---
+++ WarmUp/NestedLoop.js
@@ -0,0 +1,13 @@
+// for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         console.log('i=' + i, 'j=' + j)
+//     }
+// }
+
+
+
+for (let i = 0; i < 3; i++) {
+    for (let j = i; j > 0; j--) {
+        console.log('i=' + i, 'j=' + j)
+    }
+}
